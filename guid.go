fb84d37f4a1664379f11dc568a349eacbce14de3
Pham Kieu Thanh <thanhpk@live.com>
2017-02-24 10:28:02 +0700

    initialize commit

---
+++ guid.go
@@ -0,0 +1,28 @@
+package ranstr
+
+import(
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	if err != nil {
+		return "", err
+	}
+	
+	hexstring :=  hex.EncodeToString(b)
+
+	return hexstring, err
+}
+
